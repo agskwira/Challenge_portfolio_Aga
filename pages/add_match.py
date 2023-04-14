25e1a14d56e94171245add7d37aa8b505f28ff31
Agatestowanie <askwtestowanie@gmail.com>
2023-04-14 11:11:07 +0200

    poprawa cudzysłowi

--- pages/add_match.py
+++ pages/add_match.py
@@ -2,13 +2,13 @@ from _ast import Add
 
 
 class Dashboard(Add Match)
-steven_bell_riggs_page_hyperlink_xpath="//*[@class="jss19"]/ul[2]/div[1]/div[2]/span"
-matches_page_hyperlink_xpath="//*[@class="jss19"]/ul[2]/div[2]/div[2]/span"
-reports_page_hyperlink_xpath="//*[@class="jss19"]/ul[2]/div[3]"
-my_team_field_xpath="//*[@name="myTeam"]"
-enemy_team_field_xpath="//*[@name="enemyTeam"]"
-my_team_score_field_xpath="//*[@name="myTeamScore"]"
-match_at_home_checkbox_title_xpatch="//*[text()="Match at home"]"
-match_out_home_checkbox_title_xpatch="//*[text()="Match out home"]"
-match_at_home_checkbox_xpatch="//input[@value="true"]"
-match_out_home_checkbox_xpatch="//input[@value="false"]"
+steven_bell_riggs_page_hyperlink_xpath="//*[@class='jss19']/ul[2]/div[1]/div[2]/span"
+matches_page_hyperlink_xpath="//*[@class='jss19']/ul[2]/div[2]/div[2]/span"
+reports_page_hyperlink_xpath="//*[@class='jss19']/ul[2]/div[3]"
+my_team_field_xpath="//*[@name='myTeam']"
+enemy_team_field_xpath="//*[@name='enemyTeam']"
+my_team_score_field_xpath="//*[@name='myTeamScore']"
+match_at_home_checkbox_title_xpatch="//*[text()='Match at home']"
+match_out_home_checkbox_title_xpatch="//*[text()=' out home']"
+match_at_home_checkbox_xpatch="//input[@value='true']"
+match_out_home_checkbox_xpatch="//input[@value='false']"
--- pages/login_page.py
+++ pages/login_page.py
@@ -2,11 +2,11 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    Scouts_Panel_header_xpath="//*[@id="__next"]/form/div/div[1]/h5"
+    Scouts_Panel_header_xpath="//*[@id='__next'/form/div/div[1]/h5"
     login_field_xpath ="//*[@id='login']"
-    password_field_xpath ="//*[@id="__next"]/form/div/div[1]/div[2]/div[1]//input"
-    sign_in_button_xpath ="//*[@id="__next"]/form/div/div[2]/button//span"
-    remind_password_hyperlink_xpath ="//*[@id="__next"]/form/div/div[1]/a"
-    language_listbox_xpath="//*[@id="__next"]/form/div/div[2]/div[1]//div"
+    password_field_xpath ="//*[@id='__next']/form/div/div[1]/div[2]/div[1]//input"
+    sign_in_button_xpath ="//*[@id='__next']/form/div/div[2]/button//span"
+    remind_password_hyperlink_xpath ="//*[@id='__next']/form/div/div[1]/a"
+    language_listbox_xpath="//*[@id='__next']/form/div/div[2]/div[1]//div"
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
