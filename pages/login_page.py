529659aece642df35420307de2eca59df886a32e
Agatestowanie <askwtestowanie@gmail.com>
2023-04-17 13:41:28 +0200

    task3.1

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,11 +2,18 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    Scouts_Panel_header_xpath="//*[@id='__next'/form/div/div[1]/h5"
+    Scouts_Panel_header_xpath ="//*[@id='__next'/form/div/div[1]/h5"
     login_field_xpath ="//*[@id='login']"
     password_field_xpath ="//*[@id='__next']/form/div/div[1]/div[2]/div[1]//input"
     sign_in_button_xpath ="//*[@id='__next']/form/div/div[2]/button//span"
     remind_password_hyperlink_xpath ="//*[@id='__next']/form/div/div[1]/a"
-    language_listbox_xpath="//*[@id='__next']/form/div/div[2]/div[1]//div"
+    language_listbox_xpath ="//*[@id='__next']/form/div/div[2]/div[1]//div"
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+    def type_in_password(self,password):
+        self.field_send_keys(self.password_field_xpath, password)
+    def click_on_sign_in_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath).click()
+
+    def title_of_page(self):
+        assert self.get_page_title(self.login_url) == self.expected_title
