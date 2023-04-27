06277b40bac660124236c2dc344b67798e2b8d48
Agatestowanie <ag.skwira@gmail.com>
2023-04-27 22:59:58 +0200

    Zad 5

--- all_suite_test.py
+++ all_suite_test.py
@@ -1,15 +1,30 @@
 import unittest
 
-from unittest.loader import makeSuite
+
 
 from test_cases.login_to_the_system import TestLogInPage
-from test_cases.add_player_assert import TestAddPlayer
+from test_cases.zad4_test import TestCase01
+
 
 def full_suite():
-   test_suite = unittest.TestLoader()
-   test_suite.loadTestsFromTestCase(TestLogInPage)
-   return test_suite
+    test_loader = unittest.TestLoader()
 
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(test_loader.loadTestsFromTestCase(TestLogInPage))
+    test_suite.addTest(test_loader.loadTestsFromTestCase(TestCase01))
+
+    new_test_suite = unittest.TestSuite()
+    new_test_suite.addTests(test_suite)
+
+    test_names = []
+    for test in new_test_suite:
+        if isinstance(test, unittest.TestCase):
+            test_names.append(str(test))
+        else:
+            for sub_test in test:
+                test_names.append(str(sub_test))
+
+    unittest.TextTestRunner().run(test_suite)
 
 if __name__ == '__main__':
    runner = unittest.TextTestRunner(verbosity=2)
